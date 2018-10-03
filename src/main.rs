5f2271ac41c4bc7795c9155fefccc1fff0eb5902
ykonomi <hgKaguyaToupako4er@gmail.com>
2018-10-04 04:57:17 +0900

    Add a page

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
+extern crate actix;
 extern crate actix_web;
-use actix_web::{http, server, App, Path, Responder};
 
-fn index(info: Path<(u32, String)>) -> impl Responder {
-    format!("Hello {}! id:{}", info.1, info.0)
-}
+use actix_web::{http::Method, server::HttpServer, App, HttpRequest};
 
 fn main() {
-    server::new(
-        || App::new()
-            .route("/{id}/{name}/index.html", http::Method::GET, index))
-        .bind("127.0.0.1:8080").unwrap()
-        .run();
+    let sys = actix::System::new("guide");
+
+    HttpServer::new(|| {
+        vec![
+            App::new().resource("/index.html", |r| r.method(Method::GET).f(index)),
+            App::new().resource("/start", |r| r.method(Method::GET).f(start)),
+        ]
+    }).bind("127.0.0.1:8080")
+    .unwrap()
+    .start();
+
+    let _ = sys.run();
+}
+
+fn index(_req: &HttpRequest) -> &'static str {
+    r"
+    こちらはAMPのデモになります
+    - AMPの基本 (http://127.0.0.1:8080/first)
+    "
+}
+
+fn start(_req: &HttpRequest) -> &'static str {
+    r"
+    こちらはAMPのデモになります
+    - AMPの基本 (http://127.0.0.1:8080/first)
+    "
 }
