e98724b8c27b02d08acb3c1c8915bfeb3b8fde8b
ykonomi <hgKaguyaToupako4er@gmail.com>
2018-11-06 21:19:47 +0900

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,27 @@ extern crate askama;
 
 #[derive(Template)]
 #[template(path = "index.html")]
-
-struct HelloTemplate<'a> {
-    name: &'a str,
+struct IndexTemplate<'a> {
+    basis_url: &'a str,
 }
 
-#[derive(Template)]
-#[template(path = "article.amp.html")]
-struct BasisTemplate<'a> {
-    name: &'a str,
+use actix_web::{fs, http::Method, http::StatusCode, server, App, HttpRequest, HttpResponse, Result};
+use askama::Template;
+
+fn basis(req: &HttpRequest) -> Result<HttpResponse> {
+    // response
+    Ok(HttpResponse::build(StatusCode::OK)
+        .content_type("text/html; charset=utf-8")
+        .body(include_str!("../templates/article.amp.html")))
 }
 
-use actix_web::{fs, http::Method, server, App, HttpRequest, HttpResponse, Result};
-use askama::Template; // bring trait in scope
+
+fn index(_req: &HttpRequest) -> Result<HttpResponse> {
+    let index = IndexTemplate { basis_url: "http://localhost:8080/basis" };
+    Ok(HttpResponse::Ok()
+        .content_type("text/html")
+        .body(index.render().unwrap()))
+}
 
 fn main() {
     let sys = actix::System::new("amp-examples");
@@ -32,18 +40,4 @@ fn main() {
     .start();
 
     let _ = sys.run();
-}
-
-fn index(_req: &HttpRequest) -> Result<HttpResponse> {
-    let hello = HelloTemplate { name: "world" };
-    Ok(HttpResponse::Ok()
-        .content_type("text/html")
-        .body(hello.render().unwrap()))
-}
-
-fn basis(_req: &HttpRequest) -> Result<HttpResponse> {
-    let basis = BasisTemplate { name: "world" };
-    Ok(HttpResponse::Ok()
-        .content_type("text/html")
-        .body(basis.render().unwrap()))
 }
